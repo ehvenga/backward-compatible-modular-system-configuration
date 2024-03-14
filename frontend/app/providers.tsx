ed751d573d2f0651a228a0170ceee280487cbf40
ehvenga <ehvenga@gmail.com>
2024-03-14 17:22:25 -0400

    new frontend

--- frontend/app/providers.tsx
+++
@@ -1,5 +0,0 @@
-import { Provider } from 'jotai';
-
-export default function Providers({ children }: { children: any }) {
-  return <Provider>{children}</Provider>;
-}
--- frontend/components/atoms.tsx
+++
@@ -1,5 +0,0 @@
-import { atom } from 'jotai';
-
-export type StrategyChoices = 'dp' | 'bf' | 'cp';
-
-export const strategyChoiceAtom = atom<StrategyChoices>('dp');
