e2df04665bd111ed9dcc2fc70b1b98822e3dd7b7
ehvenga <ehvenga@gmail.com>
2024-03-20 23:49:22 -0500

    frontend

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,15 @@
 from fastapi import FastAPI
+from databases import Database
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+DATABASE_URL = "mysql://root:wag0nrM$@localhost/cs591"
+database = Database(DATABASE_URL)
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
+
+Base = declarative_base()
 
 app = FastAPI()
 
