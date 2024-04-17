e6e2ad2d91943b4654cf7fdff1061e386bbb7fae
ehvenga <ehvenga@gmail.com>
2024-04-17 12:29:59 -0500

    clear all and move all

--- frontend/app/tool/page.tsx
+++ frontend/app/tool/page.tsx
@@ -102,6 +102,16 @@ export default function ToolPage() {
     );
   };
 
+  const handleClearAllInitialParams = () => {
+    setSelectedInitialParameterList([]);
+    setToBeSelectedInitialParamList(parameterList);
+  };
+
+  const handleMoveAllInitialParams = () => {
+    setSelectedInitialParameterList(parameterList);
+    setToBeSelectedInitialParamList([]);
+  };
+
   const handleSelectedGoalParamClicked = (param) => {
     // Add the clicked parameter back to the toBeSelectedInitialParamList
     setToBeSelectedGoalParamList((prevParams) =>
@@ -116,13 +126,23 @@ export default function ToolPage() {
     );
   };
 
+  const handleClearAllGoalParams = () => {
+    setSelectedGoalParameterList([]);
+    setToBeSelectedGoalParamList(parameterList);
+  };
+
+  const handleMoveAllGoalParams = () => {
+    setSelectedGoalParameterList(parameterList);
+    setToBeSelectedGoalParamList([]);
+  };
+
   const handleCompose = () => {
     handleFindServicesChain();
   };
 
   const handleFindServicesChain = async () => {
     // setPartsChain([]);
-    // setNoSolution(false);
+    setNoSolution(false);
     const listInitial = selectedInitialParameterList.map((item) => item.value);
     const listGoal = selectedGoalParameterList.map((item) => item.value);
     try {
@@ -147,9 +167,12 @@ export default function ToolPage() {
 
       const jsonData = await response.json();
       // console.log('Submission response: ', jsonData.webServiceChains);
-      console.log(jsonData.stages);
-      const newArrays = getRandomSubsets(jsonData.stages, 20);
-      setPartsChainList(newArrays);
+      // const newArrays = getRandomSubsets(jsonData.stages, 20);
+      if (jsonData.stages[0].length > 0) {
+        setPartsChainList(jsonData.stages);
+      } else {
+        setNoSolution(true);
+      }
       // setPartsChain(jsonData.webServiceChain);
     } catch (error) {
       console.error('Error submitting selection: ', error);
@@ -159,7 +182,7 @@ export default function ToolPage() {
 
   const handleChooseService = (parts, item) => {
     console.log(parts, item);
-    setShowPartsOptions(true);
+    // setShowPartsOptions(true);
   };
 
   class SeededRandom {
@@ -217,10 +240,19 @@ export default function ToolPage() {
         </CardHeader>
         <CardContent className='flex gap-3'>
           <div className='w-1/2'>
-            <h3 className='font-medium border-b border-purple-800 mb-2'>
-              Parameter List
-            </h3>
+            <div className='flex justify-between border-b border-purple-800 mb-2 items-end'>
+              <h3 className='font-medium'>Parameter List</h3>
+              <h4 className='text-sm'>
+                {toBeSelectedInitialParamList.length} items
+              </h4>
+            </div>
             <div className='h-60 overflow-y-auto text-black rounded bg-purple-50'>
+              <h3
+                onClick={handleMoveAllInitialParams}
+                className='bg-purple-100 border-b border-b-purple-300 hover:bg-white hover:font-medium hover:text-green-900 h-8 flex justify-center items-center cursor-pointer px-2'
+              >
+                Move all &rArr;
+              </h3>
               {toBeSelectedInitialParamList.map((param, idx) => (
                 // @ts-ignore
                 <div
@@ -234,10 +266,20 @@ export default function ToolPage() {
             </div>
           </div>
           <div className='w-1/2'>
-            <h3 className='font-medium border-b border-purple-800 mb-2'>
-              Initial Parameters
-            </h3>
+            <div className='flex justify-between border-b border-purple-800 mb-2 items-end'>
+              <h3 className='font-medium'>Initial Parameters</h3>
+              <h4 className='text-sm'>
+                {selectedInitialParameterList.length} items
+              </h4>
+            </div>
+
             <div className='h-60 overflow-y-auto text-black rounded bg-purple-50'>
+              <h3
+                onClick={handleClearAllInitialParams}
+                className='bg-purple-100 border-b border-b-purple-300 hover:bg-white hover:font-medium hover:text-red-800 h-8 flex justify-center items-center cursor-pointer px-2'
+              >
+                &lArr; Clear all
+              </h3>
               {selectedInitialParameterList.map((param, idx) => (
                 // @ts-ignore
                 <div
@@ -254,10 +296,19 @@ export default function ToolPage() {
 
         <CardContent className='flex gap-3'>
           <div className='w-1/2'>
-            <h3 className='font-medium border-b border-purple-800 mb-2'>
-              Parameter List
-            </h3>
+            <div className='flex justify-between border-b border-purple-800 mb-2 items-end'>
+              <h3 className='font-medium'>Parameter List</h3>
+              <h4 className='text-sm'>
+                {toBeSelectedGoalParamList.length} items
+              </h4>
+            </div>
             <div className='h-60 overflow-y-auto text-black rounded bg-purple-50'>
+              <h3
+                onClick={handleMoveAllGoalParams}
+                className='bg-purple-100 border-b border-b-purple-300 hover:bg-white hover:font-medium hover:text-green-900 h-8 flex justify-center items-center cursor-pointer px-2'
+              >
+                Move all &rArr;
+              </h3>
               {toBeSelectedGoalParamList.map((param, idx) => (
                 // @ts-ignore
                 <div
@@ -271,10 +322,19 @@ export default function ToolPage() {
             </div>
           </div>
           <div className='w-1/2'>
-            <h3 className='font-medium border-b border-purple-800 mb-2'>
-              Goal Parameters
-            </h3>
+            <div className='flex justify-between border-b border-purple-800 mb-2 items-end'>
+              <h3 className='font-medium'>Goal Parameters</h3>
+              <h4 className='text-sm'>
+                {selectedGoalParameterList.length} items
+              </h4>
+            </div>
             <div className='h-60 overflow-y-auto text-black rounded bg-purple-50'>
+              <h3
+                onClick={handleClearAllGoalParams}
+                className='bg-purple-100 border-b border-b-purple-300 hover:bg-white hover:font-medium hover:text-red-900 h-8 flex justify-center items-center cursor-pointer px-2'
+              >
+                &lArr; Clear all
+              </h3>
               {selectedGoalParameterList.map((param, idx) => (
                 // @ts-ignore
                 <div
@@ -302,62 +362,8 @@ export default function ToolPage() {
           </div>
         </CardContent>
       </Card>
-      <div>
-        {partsChainList?.length > 0 && (
-          <Card className='mt-6 relative bg-white shadow-lg sm:rounded-3xl p-6 bg-clip-padding bg-opacity-80 border border-gray-100'>
-            <CardHeader>
-              <CardTitle className='pb-2 text-2xl border-b border-slate-400'>
-                Solution
-              </CardTitle>
-            </CardHeader>
 
-            {/* <CardContent className='flex flex-col'>
-              <h2 className='font-medium text-xl w-full'>
-                Parameters Solution
-              </h2>
-            </CardContent> */}
-
-            {partsChainList?.length > 0 && (
-              <CardContent className='flex flex-col gap-4 w-full'>
-                <h2 className='font-medium text-xl w-full'>Parts Solution</h2>
-                <div className='flex items-center gap-4 w-full flex-col'>
-                  {partsChainList.map((part, index) => (
-                    <div
-                      className='flex w-full border border-purple-800 p-4 rounded'
-                      key={index}
-                    >
-                      <p className='mr-10 w-40 font-medium my-5'>
-                        Stage {index + 1}
-                      </p>
-                      <div className='flex gap-x-2 justify-between'>
-                        {part.map((item, idx) => {
-                          productCounter += 1; // Increment the counter for each product
-                          return (
-                            <div
-                              className='flex items-center justify-center py-3 px-4 border border-purple-600 rounded hover:cursor-pointer hover:bg-purple-100'
-                              key={idx}
-                            >
-                              <div className='flex items-center justify-center flex-col'>
-                                {/* Use the persistent counter for numbering */}
-                                <p className='font-semibold'>
-                                  Product {productCounter}
-                                </p>
-                                <p>{item}</p>
-                              </div>
-                            </div>
-                          );
-                        })}
-                      </div>
-                    </div>
-                  ))}
-                </div>
-              </CardContent>
-            )}
-          </Card>
-        )}
-      </div>
-
-      {noSolution && (
+      {noSolution ? (
         <>
           <Alert className=' mt-6 bg-red-50'>
             <AlertTitle className=''>Solution Not Found</AlertTitle>
@@ -367,6 +373,64 @@ export default function ToolPage() {
             </AlertDescription>
           </Alert>
         </>
+      ) : (
+        <div>
+          {partsChainList?.length > 0 && (
+            <Card className='mt-6 relative bg-white shadow-lg sm:rounded-3xl p-6 bg-clip-padding bg-opacity-80 border border-gray-100'>
+              <CardHeader>
+                <CardTitle className='pb-2 text-2xl border-b border-slate-400'>
+                  Solution
+                </CardTitle>
+              </CardHeader>
+
+              {/* <CardContent className='flex flex-col'>
+              <h2 className='font-medium text-xl w-full'>
+                Parameters Solution
+              </h2>
+            </CardContent> */}
+
+              {partsChainList?.length > 0 && (
+                <CardContent className='flex flex-col gap-4 w-full'>
+                  <h2 className='font-medium text-xl w-full'>Parts Solution</h2>
+                  <div className='flex items-center gap-4 w-full flex-col'>
+                    {partsChainList.map((part, index) => (
+                      <div
+                        className='flex w-full border border-purple-800 p-4 rounded'
+                        key={index}
+                      >
+                        <p className='mr-10 w-40 font-medium my-5'>
+                          Stage {index + 1}
+                        </p>
+                        <div className='flex flex-wrap gap-y-2 gap-x-2 justify-between'>
+                          {part.map((item, idx) => {
+                            productCounter += 1; // Increment the counter for each product
+                            return (
+                              <div
+                                className='flex items-center justify-center py-3 px-4 border border-purple-600 rounded hover:cursor-pointer hover:bg-purple-100'
+                                key={idx}
+                                onClick={() =>
+                                  handleChooseService(partsChainList, index)
+                                }
+                              >
+                                <div className='flex items-center justify-center flex-col'>
+                                  {/* Use the persistent counter for numbering */}
+                                  <p className='font-semibold'>
+                                    Product {productCounter}
+                                  </p>
+                                  <p>{item}</p>
+                                </div>
+                              </div>
+                            );
+                          })}
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                </CardContent>
+              )}
+            </Card>
+          )}
+        </div>
       )}
     </main>
   );
